8b1ea809f021f222bfc1de9782775c78c4421276
Lakshman Karunasena <lakkaru@gmail.com>
2023-02-07 19:31:51 +0530

    feat- by unshift and push

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ console.log(arrReq1); // (7) [10, 20, 30, 40, 50, 60, 70]
 arrReq1.push(80);
 console.log(arrReq1); // (8) [10, 20, 30, 40, 50, 60, 70, 80]
 
+//Method 2 - by shift, unshift, and push
+const arrReq2=[];
+arrReq2.unshift(10);
+console.log(arrReq2); // [10]
+arr1.forEach(val=>{
+    arrReq2.push(val);
+});
+console.log(arrReq2); // (4) [10, 20, 30, 40]
+arrReq2.push(50);
+console.log(arrReq2); // (5) [10, 20, 30, 40, 50]
+arr2.forEach(val=>{
+    arrReq2.push(val);
+});
+console.log(arrReq2); // (7) [10, 20, 30, 40, 50, 60, 70]
+arrReq2.push(80);
+console.log(arrReq2); // (8) [10, 20, 30, 40, 50, 60, 70, 80]
